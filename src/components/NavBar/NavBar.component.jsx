dfe376403504029cb0c260e56e5561149598d0a6
shubhankarb180 <shubhankarb180@gmail.com>
2021-08-24 00:31:46 +0530

    style: Intro with Social Media Links completed for Mobile View

--- src/components/NavBar/NavBar.component.jsx
+++ src/components/NavBar/NavBar.component.jsx
@@ -2,16 +2,21 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <header className="flex-column w-full justify-between bg-gray-900 text-white fixed -top-0" id='header'>
+    <header
+      className="flex-column w-full justify-between bg-gray-900 text-white fixed -top-0 border-b-2 border-gray-600"
+      id="header"
+    >
       <nav className="flex justify-between" id="navigation">
         {/* left */}
         <div className="flex p-4">
-          <h3>Logo</h3>
+          <h3>Shubhankar Banerjee</h3>
         </div>
 
         {/* right */}
         <div className="flex-row space-x-4 p-4">
-          <a className='test' href="#test">Home</a>
+          <a className="test" href="#test">
+            Home
+          </a>
           <a href="#test">About</a>
           <a href="#test">Portfolio</a>
           <a href="#test">Contact</a>
